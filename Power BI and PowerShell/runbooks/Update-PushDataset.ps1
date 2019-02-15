8178170b1dc20bbea03ffa1d40432158765c1f78
Craig Porteous <Craig@CraigPorteous.com>
2019-02-15 09:29:58 +0000

    Adding runbook for Azure automation option

---
+++ Power BI and PowerShell/runbooks/Update-PushDataset.ps1
@@ -0,0 +1,11 @@
+#-------------------------------------------------------
+# Author: Craig Porteous
+# Presentation: Power BI and PowerShell: A match made in Heaven
+# Script: Azure Automation runbook script
+#-------------------------------------------------------
+
+#-------------------------------------------------------
+# Variables set up in Azure Automation account:
+# client_id
+# client_secret
+# workspace_id
